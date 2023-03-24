734fa33ab5e6648f1e826e4ef3221136926d08d8
Dawid <davidowsky9519@gmail.com>
2023-03-24 14:37:06 +0100

    button style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Button = props => {
   const { loadMoreBttn } = props;
-  return <button onClick={loadMoreBttn}>Load more</button>;
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '30px',
+      }}
+    >
+      <button
+        onClick={loadMoreBttn}
+        style={{
+          padding: '10px 30px',
+        }}
+      >
+        Load more
+      </button>
+    </div>
+  );
 };
 
 export default Button;
